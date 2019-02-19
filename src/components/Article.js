d0d07eb8641fb773ea4da95d57d090298354703a
Katie Robinson <katierobinson110589@gmail.com>
2019-02-19 15:03:39 +0000

    added Article view

--- src/components/Article.js
+++ src/components/Article.js
@@ -2,6 +2,11 @@ import React, { Component } from "react";
 import { navigate } from "@reach/router";
 import PropTypes from "prop-types";
 import { fetchArticleById, fetchData } from "../api";
+import Auth from "./Auth";
+import Vote from "./Vote";
+import CommentAdder from "./CommentAdder";
+import CommentCard from "./CommentCard";
+import "../css/Article.css";
 
 export class Article extends Component {
   state = {
@@ -31,11 +36,57 @@ export class Article extends Component {
   }
 
   render() {
-    const { article, comments, showDelete } = this.state;
+    const { article, comments, showDelete, showCommentAdder } = this.state;
     const { user, setUser } = this.props;
     const date = Date(article.created_at).slice(0, 21);
 
-    return <div>ARTICLE</div>;
+    return (
+      <Auth user={user} setUser={setUser}>
+        <div className="App-body">
+          <div className="article-header">
+            <div id="topic">{article.topic}</div>
+            <Vote article_id={article.article_id} votes={article.votes} />
+          </div>
+          <div className="article">
+            {showDelete && (
+              <div className="delete">
+                <button onClick={this.handleDelete}>Delete this article</button>
+              </div>
+            )}
+            <h1>{article.title}</h1>
+            <div className="author">written by {article.author}</div>
+            <div className="date">{date}</div>
+            {article.body}
+          </div>
+          <p>
+            <button id="add-comment" onClick={this.toggleCommentInput}>
+              Leave a comment
+            </button>
+          </p>
+          <div className="comment-input">
+            {showCommentAdder && (
+              <CommentAdder
+                article_id={article.article_id}
+                user={user}
+                updateComments={this.updateComments}
+              />
+            )}
+          </div>
+          {comments ? (
+            comments.map(comment => (
+              <CommentCard
+                key={comment.comment_id}
+                user={user}
+                comment={comment}
+                updateComments={this.updateComments}
+              />
+            ))
+          ) : (
+            <p>Loading comments...</p>
+          )}
+        </div>
+      </Auth>
+    );
   }
 }
 
